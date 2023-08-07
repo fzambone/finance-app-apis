719fc40e12793e76f514bcef916a402e1da3e6a0
fernandozambone <fernando.zambone@gmail.com>
2023-08-06 20:54:37 -0500

    Initial commit

---
+++ src/main/java/com/zambones/financeappapis/FinanceAppApisApplication.java
@@ -0,0 +1,13 @@
+package com.zambones.financeappapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAppApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAppApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zambones/financeappapis/FinanceAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zambones.financeappapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
